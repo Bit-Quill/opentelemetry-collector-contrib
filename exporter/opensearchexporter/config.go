b536bf05c4b0af52900dd8aea4b75e9440b1a408
Mitchell Gale <Mitchell.Gale@improving.com>
2023-08-28 14:30:53 -0700

    temp

--- exporter/opensearchexporter/config.go
+++ exporter/opensearchexporter/config.go
@@ -1,6 +1,6 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
-
+// nolint
 package opensearchexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/opensearchexporter"
 
 import (
